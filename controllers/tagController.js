aff4c8c898b37952b4081196332127e491c842b6
Opeyemi Akintomide <akintomide.opeyemi83@gmail.com>
2025-12-15 09:09:27 +0100

    refactor: use singleton prisma client to fix 500 errors

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -1,5 +1,4 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+const prisma = require("../prisma/client");
 
 // CREATE
 exports.createTag = async (req, res) => {
