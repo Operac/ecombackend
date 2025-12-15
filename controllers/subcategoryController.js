0f318fd09a4ab3d5df573f58604243d254b9b616
Opeyemi Akintomide <akintomide.opeyemi83@gmail.com>
2025-12-15 09:39:16 +0100

    chore: add postinstall script for prisma generate

--- controllers/subcategoryController.js
+++ controllers/subcategoryController.js
@@ -1,4 +1,5 @@
-const prisma = require("../prisma/client");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 console.log("SubcategoryController: Imported prisma. Has subcategory?", !!prisma.subcategory);
 
 // CREATE
--- controllers/tagController.js
+++ controllers/tagController.js
@@ -1,4 +1,5 @@
-const prisma = require("../prisma/client");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 // CREATE
 exports.createTag = async (req, res) => {
